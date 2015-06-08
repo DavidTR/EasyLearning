5b258971f064f22be58e1dbea4c19d22b2dee9c1
Antonio Cano Díaz <goyo.meteojorito@gmail.com>
2015-06-08 13:59:24 +0200

    Añadido crea_munyecos.js actualizado (comentarios + separación entre instanciación y movimiento)

--- New Unity Project 2/Assets/Scripts/crear_munyecos.js
+++ New Unity Project 2/Assets/Scripts/crear_munyecos.js
@@ -1,71 +1,62 @@
 ﻿#pragma strict
 import System.Text.RegularExpressions;
 
-var pref : GameObject;
-var camara : GameObject;
-var onlyNumbers1 : String;
-var onlyNumbers2 : String;
-var vector : Vector3[];
-var munyecos : GameObject[];
+var pref : GameObject;       // El prefab que entra como parametro, del que se instancian las copias
+var camara : GameObject;     // La camara AR
+var onlyNumbers1 : String;   // Entrada numerica 1
+var onlyNumbers2 : String;   // Entrada numerica 2
+var posicion : Vector3[];    // Posicion de inicio de los muñecos
+var munyecos : GameObject[]; // Vector de instancias de muñecos
+var agente : NavMeshAgent[]; // Vector de componentes NavMeshAgent de cada muñeco (replicados de prefab)
 var insertado : int = 0;
 var yaEsta : int = 0;
 
 function Start () {
-    //objeto.transform.position = Vector3 (x, y, 0);
+	posicion = new Vector3[4];
+	munyecos = new GameObject[4];
+	posicion[0] = Vector3(50, 0, -50);
+	posicion[1] = Vector3(50, 0, -150);
+	posicion[2] = Vector3(-50, 0, -50);
+	posicion[3] = Vector3(-50, 0, -150);
+
+    for(var i = 0 ; i < 4 ; i++){
+    	munyecos[i] = Instantiate(pref, posicion[i], Quaternion.identity);
+		munyecos[i].transform.parent = camara.transform;                         // Todos los muñecos son hijos de la camara AR
+		yield WaitForSeconds(1);                                               // Retraso en la instanciacion entre muñecos
+	}
+	
+	yaEsta = 1;
 }
 
 function Update () {
-	if(insertado){
-		InstanciarYretrasar();
-		insertado = 0;
-		yaEsta = 1;
-	}
-	
 	if(yaEsta) Mover();
-	
-	
 }
 
 function OnGUI() {
 	onlyNumbers1= GUI.TextField(new Rect(30, 110, 30, 20), onlyNumbers1, 3);
-	onlyNumbers1= Regex.Replace(onlyNumbers1, "[^0-9]", "");
+	onlyNumbers1= Regex.Replace(onlyNumbers1, "[^0-9]", "");                      // Exp. regular solo para numeros
 	
 	onlyNumbers2= GUI.TextField(new Rect(110, 190, 30, 20), onlyNumbers2, 3);
-	onlyNumbers2= Regex.Replace(onlyNumbers2, "[^0-9]", "");
+	onlyNumbers2= Regex.Replace(onlyNumbers2, "[^0-9]", "");                      // Exp. regular solo para numeros
 	
 	if (GUI.Button(new Rect(30, 220, 50, 30), "Click") && !yaEsta)
        insertado = 1;
 }
 
-function InstanciarYretrasar(){
-	vector = new Vector3[4];
-	munyecos = new GameObject[4];
-	vector[0] = Vector3(50, 0, 50);
-	vector[1] = Vector3(50, 0, -50);
-	vector[2] = Vector3(-50, 0, 50);
-	vector[3] = Vector3(-50, 0, -50);
-	for(var i = 0 ; i < vector.length ; i++){	
-		munyecos[i] = Instantiate(pref, vector[i], Quaternion.identity) as GameObject;
-		munyecos[i].transform.parent = camara.transform;
-		yield WaitForSeconds(2);
-	}
-}
-
 function Mover(){
-	var speed : float = 50.0;
+	agente = new NavMeshAgent[4];
 	var target : Vector3;
 	var x : int;
 	var y : int;
 	var z : int;
 	
-	var step : float = speed * Time.deltaTime;
-	
-	for(var i = 0 ; i < vector.length ; i++){
-		x = munyecos[i].transform.position.x;
-		y = munyecos[i].transform.position.y;
-		z = munyecos[i].transform.position.z;
+	for(var i = 0 ; i < 4 ; i++){
+		agente[i] = munyecos[i].GetComponent.<NavMeshAgent>();                   // Obtencion del componente NavMeshAgent de cada muñeco
+		x = posicion[i].x;
+		y = posicion[i].y;
+		z = posicion[i].z + 180;
 		target = new Vector3(x, y, z);
-		munyecos[i].transform.position = Vector3.MoveTowards(munyecos[i].transform.position, new Vector3(x, y, z+10), step);
+		agente[i].SetDestination(target);                                        // Destino del componente NavMeshAgent
+		yield WaitForSeconds(1);                                                // Retraso al comenzar a moverse hacia el destino
 	}
 }
-	
